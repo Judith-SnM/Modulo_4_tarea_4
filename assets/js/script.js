b4887c3b91168a6aa1b210df5bd946202b2220fa
Judith <judith.sanmartin.s@gmail.com>
2024-04-04 17:04:39 -0300

    uso del retraso tiempo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 //funcion iife
 const iifeDom = (() => {
     function funcionPrivada(url, id) {
-        document.querySelector(`#${id}`).setAttribute("scr", url)
+        document.querySelector(`#${id}`).setAttribute("src", url)
     }
     return {
         funcionPublica(url, id) {
@@ -36,14 +36,20 @@ class Reproductor extends Multimedia {
     playMultimedia() {
         iifeDom.funcionPublica(this.url, this._id)
     }
-    setInicio() {
-
-    }
+    setInicio(tiempo) {
+        const urlRetrasado= `${this.url}?start=${tiempo}`;
+        iifeDom.funcionPublica(urlRetrasado, this._id);
+        
+     }
 }
 
-const musica = new Reproductor("https://www.youtube.com/watch?v=ZSM3w1v-A_Y","musica")
+const musica = new Reproductor("https://www.youtube.com/watch?v=A18ciouGCS4","musica")
 const pelicula = new Reproductor("https://www.youtube.com/watch?v=9N7EKTm4oaQ", "peliculas")
 const serie = new Reproductor("https://www.youtube.com/watch?v=LgYgY7xmzRM", "series")
+
+musica.setInicio(30)
+pelicula.setInicio(30)
+serie.setInicio(30)
 
 musica.playMultimedia()
 pelicula.playMultimedia()
